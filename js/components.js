288568eef153c3330cae60f42027b0c2c75ea452
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-04-06 22:10:58 -0500

    view updated

--- js/components.js
+++ js/components.js
@@ -46,9 +46,98 @@ class CustomImage {
      * @param {string} b64 - image source on base64 format.
      */
     setBase64Source(b64){
-        if(!b64.includes('data:')){
-           b64 = addMIMEjpeg(b64);
-        }
+        if(!b64){ return };
+        b64 = addMIMEjpeg(b64);
         this.setSource(b64);
+    }
+}
+
+class Base{
+    constructor(elementId){
+        this.element = document.getElementById(elementId);
+    }
+
+    on(event, callback){
+        if(this.element){
+            this.element.addEventListener(event, callback);
+        }
+    }
+
+    setEnabled(enabled){
+        if(this.element){
+            this.element.disabled = !enabled;
+        }
+    }
+
+    value(){
+        if(this.element){
+            return this.element.value;
+        }
+        return null;
+    }
+
+}
+
+class CustomButton extends Base {
+    constructor(elementId){
+        super(elementId);
+    }
+}
+
+class ToogleButton extends Base{
+    constructor(elementId, onStyle, offStyle) {
+        super(elementId);
+        this.checked = false;
+        this.onStyle = onStyle;
+        this.offStyle = offStyle;
+        this.on("click", this.toogle.bind(this));
+        this.updateStyle();
+    }
+
+    setChecked(checked){
+        this.checked = checked;
+        this.updateStyle();
+    }
+
+    isChecked(){
+        return this.checked;
+    }
+
+
+    updateStyle(){
+        if(this.element){
+           if(this.isChecked()){
+               this.element.className = this.onStyle;
+           }else{
+               this.element.className = this.offStyle;
+           }
+        }
+    }
+
+    toogle(){
+        this.checked = !this.checked;
+        this.updateStyle()
+    }
+}
+
+class CustomSelect extends Base {
+    constructor(elementId){
+        super(elementId);
+        this.items = [];
+    }
+
+    clear(){
+        if(this.element){
+            this.element.innerHTML = '';
+        }
+    }
+
+    setItems(items){
+        if(!items) return null;
+        if(this.element){
+            for(var i in items){
+                this.element.innerHTML+=`<option>${items[i]}</option>`
+            }
+        }
     }
 }
