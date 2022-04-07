1fd899f03decaf8ab7efd0de8fe4e54f02be56c5
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-04-06 22:10:58 -0500

    view updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,91 @@
+
+
+
+// ================== SOME VARS ================
 const address = env.SERVER_ADDRESS || "";
-const socket = io(address);
+let socket = io(address);
 const session = {
     "room": "experiment-x",
 }
+const image = new CustomImage("image");
+const connectSerialBtn = new ToogleButton("connectSerialBtn", "button is-light", "button is-light");
+const updateSerialBtn = new CustomButton("updateSerialBtn");
+const portSelect = new CustomSelect("portSelect");
+const btn1 = new ToogleButton("btn1", "button is-light", "button is-black");
+const btn2 = new ToogleButton("btn2", "button is-light", "button is-black");
+const btn3 = new ToogleButton("btn3", "button is-light", "button is-black");
+const ledSocket = new ToogleButton("ledSocket", "led led-on mx-2", "led led-off mx-2");
+const ledSerial = new ToogleButton("ledSerial", "led led-on mx-2", "led led-off mx-2");
+const serverConnectBtn = new CustomButton("serverConnectBtn");
+const serverAddressInput = document.getElementById("serverAddressInput");
+ledSocket.setEnabled(false);
+ledSerial.setEnabled(false);
 
-socket.on('connect', () => {
-    console.log('Connected...');
-    socket.emit("join", session);
+connectSerialBtn.on("click", () => {
+    let event = null;
+    if(!ledSocket.isChecked()){
+        event = {"serial": {"connect": portSelect.value()}}
+    }else{
+        event = {"serial": {"disconnect": null}};
+    }
+    if(event){
+        socket.emit(EVENT_WEB_SERVER, event);
+    }
+
 })
 
-socket.on('disconnect', () => {
-    console.log('Disconnected...');
-})
+updateSerialBtn.on("click", ()=>{
+    const event = {"serial": {"ports": []}}
+    socket.emit(EVENT_WEB_SERVER, event);
+});
+
+function configure(socket){
+        // ================ SOCKETIO EVENTS ================
+    console.log("configuring...")
+    socket.on("connect", () => {
+        ledSocket.setChecked(true);
+    });
+
+    socket.on("disconnect", () => {
+        ledSocket.setChecked(false);
+    });
+
+    socket.on(STREAM_SERVER_WEB, (data) => {
+        const { webcam } = data;
+        image.setBase64Source(webcam);
+    });
+
+    socket.on(EVENT_SERVER_WEB, (event) => {
+        if(event.serial){
+            if(event.serial.ports){
+                portSelect.clear();
+                portSelect.setItems(event.serial.ports);
+            }
+
+            if(event.serial.connection){
+                ledSerial.setChecked(event.serial.connection);
+            }
+
+        }
+    });
+}
+
+configure(socket);
+
+
+serverConnectBtn.on("click", () => {
+    const server = serverAddressInput.value;
+    console.log("connecting to: ", server);
+    if(socket){
+        if(socket.connected){
+            socket.disconnect();
+            socket = null;
+        }else{
+            socket = io(server);
+            configure(socket);
+        }
+    }else{
+        socket = io(server);
+        configure(socket);
+    }
+});
---
+++ src/js/routes.js
@@ -0,0 +1,12 @@
+const STREAM_CLIENT_SERVER = "stream_client_server"
+const STREAM_SERVER_WEB = "stream_server_web"
+
+const DATA_WEB_SERVER = "data_web_server"
+const DATA_SERVER_WEB = "data_server_web"
+const DATA_SERVER_CLIENT = "data_server_client"
+const DATA_CLIENT_SERVER = "data_client_server"
+
+const EVENT_CLIENT_SERVER = "event_client_server"
+const EVENT_SERVER_CLIENT = "event_server_client"
+const EVENT_SERVER_WEB = "event_server_web"
+const EVENT_WEB_SERVER = "event_web_server"
